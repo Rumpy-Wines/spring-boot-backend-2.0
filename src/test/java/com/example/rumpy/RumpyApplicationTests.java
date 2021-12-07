cfb8e9f0d87d45ec889f1dec0e62ee709613fe39
Oyinbo David Bayode <dev.davexoyinbo@gmail.com>
2021-12-07 02:51:15 +0100

    .

--- src/test/java/com/example/rumpy/RumpyApplicationTests.java
+++ src/test/java/com/example/rumpy/RumpyApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RumpyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
